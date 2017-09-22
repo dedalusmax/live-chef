8a1e347da121ba005aef104befc2a89a766a37bb
dedalusmax <rc@ekobit.corp>
2017-09-22 14:54:39 +0200

    adding new modules (admin and cooking)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CookingModule } from './cooking/cooking.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CookingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cooking/cooking.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CookingRoutingModule } from './cooking-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CookingRoutingModule
+  ],
+  declarations: []
+})
+export class CookingModule { }
